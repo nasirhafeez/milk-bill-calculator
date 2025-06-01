cd4f275d0fb0d56a46f3832beb7ee41904d588ec
Nasir Hafeez <38311694+nasirhafeez@users.noreply.github.com>
2025-06-01 13:58:16 +0500

    favicon

--- milkman-app/pages/_app.js
+++ milkman-app/pages/_app.js
@@ -1,5 +1,14 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Dairy Bill Calculator</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
